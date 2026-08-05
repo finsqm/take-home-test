5add52dc72c32b67219131fd027e61428e9ac6c7
Finlay McAfee <finsqm@gmail.com>
2026-08-05 18:18:44 +0100

    docker compose for running locally

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
 import app from "./app";
+import { up } from "./db/migrate";
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-	console.log(`Server is running on http://localhost:${PORT}`);
+async function main(): Promise<void> {
+	await up(process.env.DATABASE_URL as string);
+
+	app.listen(PORT, () => {
+		console.log(`Server is running on http://localhost:${PORT}`);
+	});
+}
+
+main().catch((err) => {
+	console.error("Failed to start server", err);
+	process.exit(1);
 });
